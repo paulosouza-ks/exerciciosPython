7cae71fe2a2c245cac0a1d294ad335e2c00ca376
Paulo Souza <paulo.souza@kingstarcolchoes.com.br>
2024-12-30 11:58:20 -0300

    Ex 1 de Python
    Concatenar dois valores em uma string

---
+++ concat.py
@@ -0,0 +1,8 @@
+# Receba dois dados do usuário e os concatene em uma única string
+
+d1 = input("Informe o primeiro valor: ")
+d2 = input("Informe o segundo valor: ")
+
+concat = d1 + " - " + d2
+
+print("O valor concatenado é: ", concat)
