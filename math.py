e02c182b29b5eb88f27670cdafb3a561fb906b3f
Paulo Souza <paulo.souza@kingstarcolchoes.com.br>
2024-12-30 12:25:43 -0300

    Ex 3 de Python
    Operações matemáticas com dois números

---
+++ math.py
@@ -0,0 +1,15 @@
+# Solicite dois números e informe as operações matemáticas com eles
+
+x = int(input("Digite o primeiro valor: "))
+y = int(input("Digite o segundo valor: "))
+
+vAdd = x + y
+vSubt = x - y
+vMult = x * y
+vDiv = x / y
+
+print("Seguem as operações matemáticas envolvendo os dois números: ")
+print("Adição: ", vAdd)
+print("Substração", vSubt)
+print("Multiplicação: ", vMult)
+print("Divisão: ", vDiv)
